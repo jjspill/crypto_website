fbf6433c267487c6685f065c2016ffc1aea6a44e
James Spillmann <james@spacinsider.com>
2024-08-06 01:22:19 -0600

    converting times to est, some cosmetic changes

--- app/api/route.ts
+++ app/api/route.ts
@@ -16,13 +16,41 @@ if (!dbName) {
 }
 
 export async function GET(request: Request): Promise<Response> {
+  console.log('GET /api');
   const { client, db } = await connectToDatabase(uri, dbName);
   const data = await db.collection('Data').find().toArray();
   data.sort((a, b) => b.timestamp.localeCompare(a.timestamp));
+  data.forEach((item) => {
+    Object.keys(item).forEach((key: string) => {
+      if (key.includes('_time')) {
+        item[key] = formatTimestampForExcelEastern(item[key]);
+      }
+    });
+  });
   return new Response(JSON.stringify(data), {
     // return new Response(JSON.stringify([]), {
     headers: {
       'Content-Type': 'application/json',
     },
   });
+}
+
+function formatTimestampForExcelEastern(timestamp: number): string {
+  const date = new Date(timestamp);
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: 'America/New_York', // Eastern Time zone
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  };
+
+  const formattedDate = date.toLocaleString('en-US', options);
+  return formattedDate.replace(
+    /(\d{2})\/(\d{2})\/(\d{4}), (\d{2}):(\d{2}):(\d{2})/,
+    '$3-$1-$2 $4:$5:$6',
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { unparse } from 'papaparse';
 import { JSONTree } from 'react-json-tree';
+import styles from './website.module.css';
 
 interface DataItem {
   _id: string;
@@ -22,10 +23,12 @@ export default function Home() {
     const csv = unparse(data);
     const blob = new Blob([csv], { type: 'text/csv' });
     const url = window.URL.createObjectURL(blob);
+    const date = new Date();
+
     const a = document.createElement('a');
     a.setAttribute('hidden', '');
     a.setAttribute('href', url);
-    a.setAttribute('download', 'download.csv');
+    a.setAttribute('download', getFilename());
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
@@ -39,8 +42,9 @@ export default function Home() {
         {data.length > 0 && (
           <>
             <button
-              className="border-2 border-green-400 text-white p-2 m-10 rounded-lg"
+              className={`${styles.creepInBg} border-2 border-green-400 text-white p-2 m-10 rounded-lg`}
               onClick={downloadCSV}
+              type="button"
             >
               Download CSV
             </button>
@@ -82,3 +86,14 @@ const myCustomTheme = {
   base0E: '#ae81ff',
   base0F: '#cc6633',
 };
+
+function getFilename() {
+  const date = new Date();
+
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed in JS, add 1
+  const day = date.getDate().toString().padStart(2, '0');
+  const hour = date.getHours().toString().padStart(2, '0');
+  const minute = date.getMinutes().toString().padStart(2, '0');
+
+  return `data_${month}_${day}_${hour}_${minute}.csv`;
+}
